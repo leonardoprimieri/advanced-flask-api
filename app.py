0d99e7fdbbc6e226e032b013b952f1b64ef9bfbf
Leonardo Merlo Primieri <leonardo.mprimieri@gmail.com>
2025-08-29 22:09:09 -0300

    feat: login user

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from database import db
 from models.user import User
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
-@app.route('/', methods=['GET'])
-def hello():
-    return 'oi'
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({"message": "Invalid credentials!"}), 400
+
+    found_user = User.query.filter_by(username=username).first()
+    is_password_equal = found_user.password == password
+    if found_user and is_password_equal:
+        login_user(found_user)
+        return jsonify({"message": "Done!"})
+    return jsonify({"message": "Invalid credentials!"}), 400
 
 
 if __name__ == '__main__':
