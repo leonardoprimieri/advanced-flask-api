e0515499ebb737cc9bbced4da378620b1e3e0dc1
Leonardo Merlo Primieri <leonardo.mprimieri@gmail.com>
2025-08-31 20:37:22 -0300

    refactor: improve login method logic

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_login import LoginManager, login_user
+
 from database import db
 from models.user import User
-from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super_secret_key'
@@ -11,9 +12,12 @@ login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -25,10 +29,11 @@ def login():
         return jsonify({"message": "Invalid credentials!"}), 400
 
     found_user = User.query.filter_by(username=username).first()
-    is_password_equal = found_user.password == password
-    if found_user and is_password_equal:
+
+    if found_user and found_user.password == password:
         login_user(found_user)
-        return jsonify({"message": "Done!"})
+        return jsonify({"message": "Done!"}), 200
+
     return jsonify({"message": "Invalid credentials!"}), 400
 
 
